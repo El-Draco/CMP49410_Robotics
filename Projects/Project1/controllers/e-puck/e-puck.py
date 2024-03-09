69dec8de18f81b318625151fdcd493f3d4540ad0
El-Draco <radiriyas@gmail.com>
2024-03-09 16:53:02 +0400

    feat: add gps & obstacle avoidance

--- Projects/Project1/controllers/e-puck/e-puck.py
+++ Projects/Project1/controllers/e-puck/e-puck.py
@@ -18,18 +18,19 @@
 
 from controller import Robot
 
+MAX_SPEED = 6.28
 
 class Controller(Robot):
     SPEED = 6
     timeStep = 64
-
     def __init__(self):
+        
         super(Controller, self).__init__()
 
-        self.ds0 = self.getDevice('ds0')
-        self.ds1 = self.getDevice('ds1')
-        self.ds0.enable(self.timeStep)
-        self.ds1.enable(self.timeStep)
+        self.ps0 = self.getDevice('ps0')
+        self.ps1 = self.getDevice('ps1')
+        self.ps0.enable(self.timeStep)
+        self.ps1.enable(self.timeStep)
 
         self.gps = self.getDevice('gps')
         self.gps.enable(self.timeStep)
@@ -42,7 +43,10 @@ class Controller(Robot):
         self.right_motor.setPosition(float('inf'))
         self.left_motor.setVelocity(0.0)
         self.right_motor.setVelocity(0.0)
-
+        
+        
+        self.keyboard = self.getKeyboard()
+        self.keyboard.enable(self.timeStep)
 
     def run(self):
         print("Press 'G' to read the GPS device's position")
@@ -57,26 +61,34 @@ class Controller(Robot):
                 speed_vector_values = self.gps.getSpeedVector()
                 print(f'GPS speed vector: {speed_vector_values[0]} {speed_vector_values[1]} {speed_vector_values[2]}')
 
-            ds0_value = self.ds0.getValue()
-            ds1_value = self.ds1.getValue()
-            if ds1_value > 500:
+            ps0_value = self.ps0.getValue()
+            ps1_value = self.ps1.getValue()
+            if ps1_value > 500:
                 # If both distance sensors are detecting something, this means that
                 # we are facing a wall. In this case we need to move backwards.
-                if ds0_value > 200:
+                if ps0_value > 200:
                     left_speed = -self.SPEED / 2
                     right_speed = -self.SPEED
                 else:
                     # we turn proportionnaly to the sensors value because the
                     # closer we are from the wall, the more we need to turn.
-                    left_speed = -ds1_value / 100
-                    right_speed = (ds0_value / 100) + 0.5
-            elif ds0_value > 500:
-                left_speed = (ds1_value / 100) + 0.5
-                right_speed = -ds0_value / 100
+                    left_speed = -ps1_value / 100
+                    right_speed = (ps0_value / 100) + 0.5
+            elif ps0_value > 500:
+                left_speed = (ps1_value / 100) + 0.5
+                right_speed = -ps0_value / 100
             else:  # if nothing was detected we can move forward at maximal speed.
                 left_speed = self.SPEED
                 right_speed = self.SPEED
 
+            if left_speed > MAX_SPEED:
+                left_speed = MAX_SPEED
+            if right_speed > MAX_SPEED:
+                right_speed = MAX_SPEED
+            if left_speed < -MAX_SPEED:
+                left_speed = -MAX_SPEED
+            if right_speed < -MAX_SPEED:
+                right_speed = -MAX_SPEED
             self.left_motor.setVelocity(left_speed)
             self.right_motor.setVelocity(right_speed)
 
